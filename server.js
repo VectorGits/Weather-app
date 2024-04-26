a635447095be572daa532a57811da46bada3c670
VectorGits <davidoluremi0@gmail.com>
2024-04-26 17:04:07 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,44 @@
+import http from 'http';
+import fs from 'fs/promises';
+import url from 'url';
+import path from 'path';
+const PORT = process.env.PORT || 5000;
+
+// Get curr path
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// console.log(__dirname, __filename); 
+
+const server = http.createServer(async(req, res) => {
+	try {
+		// Check if get request
+		if (req.method === 'GET') {
+			let filePath;
+			if (req.url === '/'){
+				filePath = path.join(__dirname, 'public', 'index.html');
+			} else if (req.url === '/about') {
+				filePath = path.join(__dirname, 'public', 'about.html');
+			} else {
+				throw new Error('Page not found');
+			}
+
+			const data = await fs.readFile(filePath);
+			res.setHeader('Content-Type', 'text/html');
+			res.write(data);
+			res.end();
+		} else {
+			throw new Error('Invalid request method');
+		}
+	} catch (error) {
+		res.writeHead(500, { 'Content-Type': 'text/html' });
+		res.end('Server Error: ' + error.message);
+	}
+
+	
+
+})
+
+server.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
+})
